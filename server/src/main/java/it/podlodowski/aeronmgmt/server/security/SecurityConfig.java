05c988848ae98b9152ec460903c61d77dc1bc7f2
Marcin Podlodowski <marcin@podlodowski.it>
2026-02-20 12:55:30 +0100

    feat: add basic auth with UserDetailsService

--- server/src/main/java/it/podlodowski/aeronmgmt/server/security/SecurityConfig.java
+++ server/src/main/java/it/podlodowski/aeronmgmt/server/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package it.podlodowski.aeronmgmt.server.security;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +33,16 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable());
         }
         return http.build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "aeron.management.server.auth.type", havingValue = "basic")
+    public UserDetailsService userDetailsService() {
+        var user = User.withDefaultPasswordEncoder()
+                .username(authProperties.getBasic().getUsername())
+                .password(authProperties.getBasic().getPassword())
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
